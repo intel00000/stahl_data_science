398d7b935236a39a50d033d547ba618c8ed73e95
Tom Tan <29201606+intel00000@users.noreply.github.com>
2024-12-19 15:25:59 -0600

    update foldername

--- DFT_Calculations/2.preprocess/convert_naming_convention.py
+++ DFT_Calculations/2.preprocess/convert_naming_convention.py
@@ -1,10 +1,9 @@
 import os
-import json
 import pandas as pd
 import argparse
 
-# This script converts the com file from Tom naming convention (filename by common structure, for example pyrd1, pyrd2...)
-# to Leah naming convention Het001, Het002, Het003...
+# This script converts the com file from Internal naming convention (filename by common structure, for example pyrd1, pyrd2...)
+# to publication naming convention Het001, Het002, Het003...
 # The mapping is defined in the excel file named smiles_with_mapping.xlsx
 
 # for each file, split the filename by '_' and check if the prefix has a match in the mapping dictionary
@@ -13,20 +12,25 @@ import argparse
 # write to the new file
 
 
-def main(original_folder, new_folder, file_extension):
-    print(
-        f"Converting files in {original_folder} from Tom to Leah naming convention, saving to {new_folder}."
-    )
+def main(
+    original_folder,
+    new_folder,
+    file_extension,
+    mapping_file,
+    input_naming_convention_column,
+    output_naming_convention_column,
+):
+    print(f"Converting files in {original_folder} from Internal to Publication naming convention, saving to {new_folder}.")
     # Read in the mapping from smiles_with_mapping.xlsx
-    df = pd.read_excel("smiles_with_mapping.xlsx", header=0)
-    df = df[["id", "mapping"]]
+    df = pd.read_excel(mapping_file, header=0)
+    df = df[[input_naming_convention_column, output_naming_convention_column]]
 
     # Convert the mapping column to a dictionary with id being key, mapping being value
-    mapping_dict_from_tom_to_leah = dict(zip(df["id"], df["mapping"]))
+    mapping_dict_from_tom_to_leah = dict(zip(df[input_naming_convention_column], df[output_naming_convention_column]))
 
     # Dump the dictionary to a json file
-    with open("tom_to_leah_mapping.json", "w") as f:
-        json.dump(mapping_dict_from_tom_to_leah, f, indent=2)
+    # with open("tom_to_leah_mapping.json", "w") as f:
+    #     json.dump(mapping_dict_from_tom_to_leah, f, indent=2)
 
     # Grep all the files in the current directory, limited to the desired file extension
     files = [
@@ -54,18 +58,12 @@ def main(original_folder, new_folder, file_extension):
                 with open(os.path.join(original_folder, file), "r") as f:
                     with open(os.path.join(new_folder, new_filename), "w") as nf:
                         for line in f:
-                            nf.write(
-                                line.replace(
-                                    prefix, mapping_dict_from_tom_to_leah[prefix]
-                                )
-                            )
+                            nf.write(line.replace(prefix, mapping_dict_from_tom_to_leah[prefix]))
                 print(f"{file} -> {new_filename}")
             else:
                 print(f"{file} not found in mapping dictionary.")
         else:
-            print(
-                f"{file} is not in the expected naming convention (should contain an underscore)."
-            )
+            print(f"{file} is not in the expected naming convention (should contain an underscore).")
 
 
 if __name__ == "__main__":
@@ -74,15 +72,43 @@ if __name__ == "__main__":
         description="Convert and rename files based on a predefined mapping."
     )
     parser.add_argument(
-        "original_folder", type=str, help="The folder containing the original files."
+        "input_folder",
+        type=str,
+        help="The folder containing the input COM files."
     )
     parser.add_argument(
-        "new_folder", type=str, help="The folder to save renamed files."
+        "output_folder",
+        type=str,
+        help="The folder to save the renamed output COM files.",
     )
     parser.add_argument(
-        "file_extension", type=str, help="The file extension to filter (e.g., '.com')."
+        "file_extension",
+        type=str,
+        help="The file extension to filter (e.g., '.com')."
+    )
+    parser.add_argument(
+        "mapping_file",
+        type=str,
+        help="The Excel file containing the mapping."
+    )
+    parser.add_argument(
+        "input_naming_convention_column",
+        type=str,
+        help="The header of the column in the mapping file that contains the input naming convention.",
+    )
+    parser.add_argument(
+        "output_naming_convention_column",
+        type=str,
+        help="The header of the column in the mapping file that contains the output naming convention.",
     )
 
     args = parser.parse_args()
 
-    main(args.original_folder, args.new_folder, args.file_extension)
+    main(
+        args.input_folder,
+        args.output_folder,
+        args.file_extension,
+        args.mapping_file,
+        args.input_naming_convention_column,
+        args.output_naming_convention_column,
+    )
